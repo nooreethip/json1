70763959740b4a9d92da6729f2e648b76764cb06
nooreethip <86452749+nooreethip@users.noreply.github.com>
2021-12-07 10:49:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-nooree thipyorlaeh
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+
+<button id="btnJSON1"> JSON 1 </button>
+
+<body>
+
+</body>
+<script>
+    function loadJSON() {
+        var url = "http://cj-android-demon.herokuapp.com/json1.php";
+        $.get(url, (data, status) => {
+            console.log(data);
+        });
+    }
+    $(() => {
+        $("btnJSON1").click(loadJSON);
+
+    });
+</script>
+
+</html>

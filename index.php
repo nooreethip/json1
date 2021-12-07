eb2923ca45325d97bc2f4d76d8caef5791306af9
nooreethip <86452749+nooreethip@users.noreply.github.com>
2021-12-07 11:51:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
-    <button id="btnJSON1"> JSON 1 </button>
-    fname : <span id="fname"</span><br/>
-    lname : <span id="lname"</span>
-
-<body>
-
 <body>
  
     <table>
@@ -32,18 +23,25 @@
  
 function loadJSON(){
     var url = "https://cj-android-demon.herokuapp.com/json2.php";
-
     $.getJSON(url)
         .done((data)=>{
             console.log(data);
+            $.each(data,  (k, item)=>{
+                console.log(k);
+                console.log(item);
+                var line = "<tr>";
+                    line += "<td>" + (k+1) + "</td>";
+                    line += "<td>" + item.fname + "</td>";
+                    line += "<td>" + item.lname + "</td>";
+                    line += "</tr>";
+                $("#tblStudent").append(line);
+            });
         })
         .fail((xhr, status, err)=>{
-
         });
 }
-
 $(()=>{
     loadJSON();
 });
 </script>
-
+</html>
